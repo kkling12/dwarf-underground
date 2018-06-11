bb9fdbe9cd217926fdfa7e471ec3f4be8b80ed71
Kendall Kling <kkling@uncc.edu>
2018-06-11 17:56:14 -0400

    Added more components for the sub articles

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,8 @@ import Article from './Article'
 import Add from './Add'
 import SubArticle1 from './SubArticle1'
 import SubArticle2 from './SubArticle2'
+import SubArticle3 from './SubArticle3'
+import SubArticle4 from './SubArticle4'
 
 class Main extends Component {
     render() {
@@ -16,18 +18,8 @@ class Main extends Component {
                     <h2>From around the Realm</h2>
                     <SubArticle1 />
                     <SubArticle2 />
-                    <div className="small-6 medium-3 columns other-article">
-                    <a href="#">
-                        <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-                        <p>Mine 20% more gold with One Weird Trick</p>
-                    </a> 
-                    </div>
-                    <div className="small-6 medium-3 columns other-article">
-                    <a href="#">
-                        <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-                        <p>Surprise for Indiana Hobbits born before 1999</p>
-                    </a> 
-                    </div>
+                    <SubArticle3 />
+                    <SubArticle4 />
                 </div>
             </main>
         )
